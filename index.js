79734c4fc49668678b4bc86d6bebc39c797012a2
Shadi Akiki <shadiakiki1986@gmail.com>
2019-04-15 08:50:11 +0000

    enhancement: instead of live updates of the graph while editing the textarea, stages with "plot" buttons

--- index.js
+++ index.js
@@ -11,17 +11,24 @@ var lt = new LambdaTerms()
 // set options of dropdown
 // https://stackoverflow.com/a/9895164/4126114
 var jsExamplesOpt = [
-  {"lambda": "λu.x", "javascript": "u=>x", "default": true},
-  {"lambda": "λn.n(λu.x)", "javascript": "n=> n (u=>x)"},
-  {"lambda": "three := λf.λx.f(f(f x))", "javascript": "f => x => f(f(f(x)))"},
-  {"lambda": "PRED := λn.λf.λx.n (λg.λh.h (g f)) (λu.x) (λu.u)", "javascript": `n => f => x => {
+  {"title": "λu.x", "lambda": "λu.x", "javascript": "u=>x", "default": true},
+  {"title": "λn.n(λu.x)", "lambda": "λn.n(λu.x)", "javascript": "n=> n (u=>x)"},
+  {"title": "three",
+   "lambda": "three := λf.λx.f(f(f x))",
+   "javascript": "f => x => f(f(f(x)))"
+  },
+  {"title": "predecessor",
+   "lambda": "PRED := λn.λf.λx.n (λg.λh.h (g f)) (λu.x) (λu.u)",
+   "javascript": `n => f => x => {
         var L4 = u1 => x
         var L5 = u2 => u2 // notice that this is u2 (versus u1 above) instead of plain "u"
         var L6 = g => h => h(g(f))
         return ((n(L6))(L4))(L5)
       }`
   },
-  {"lambda": "PRED(three)", "javascript": `PRED3 => {
+  {"title":"predecessor(three)",
+   "lambda": "PRED(three)", 
+   "javascript": `PRED3 => {
       
         var PRED = n => f1 => x1 => {
           var L4 = u1 => x1
@@ -35,7 +42,7 @@ var jsExamplesOpt = [
         return PRED(three)
       }`
   },
-  {"lambda": "other", "javascript": ""}
+  {"title": "other", "lambda": "other", "javascript": ""}
 ]; 
 
 
@@ -45,47 +52,21 @@ var app = new Vue({
   data: {
     "jsExamplesOpt": jsExamplesOpt,
     "jsExSelected": jsExamplesOpt[0],
+    "jsOut": "",
     "dotFrom": "lambda",
-    "dotOut": "",
+    "dotOut1": "",
+    "dotOut2": "",
     "graphOut": ""
   },
   
-  // https://vuejs.org/v2/guide/computed.html#Computed-vs-Watched-Property
-  computed: {
+  methods: {
+    processJs: function() {
+      this.jsOut = this.jsExSelected.javascript;
+    },
     
-    "dotFromLambda": function () {
-      if(this.jsExSelected.javascript == "") { return }
-      
-      var dot_in = this.jsExSelected.javascript;
-      //dot_in = 'digraph { a -> b }' // for testing
-      //dot_in = pred_arrow_dot // for testing
-      
-      try {
-        dot_in = eval(dot_in);
-        var lambda_dict = lr.jsjson2dict(esprima.parse(dot_in.toString()));
-        console.log("lambda dict", lambda_dict)
-        var lambda_dot = lr.dict2dot_main(lambda_dict)
-        console.log("lambda dot", lambda_dot)
-      }
-      catch (e) {
-        // statements to handle any exceptions
-        document.getElementById("graph_out").innerHTML = e;
-        return
-      }
-      
-      this.dotOut = lambda_dot;
-      return lambda_dot;
-      //document.getElementById("dot_out").innerHTML = lambda_dot;
-      //dotChange()
-    }
-    
-  },
-  
-  watch: {
-    
-    "dotOut": function() {
-      console.log("watching dotout")
-      var lambda_dot = this.dotOut; // document.getElementById("dot_out").innerHTML;
+    processDot: function() {
+
+      var lambda_dot = this.dotOut2; // document.getElementById("dot_out").innerHTML;
 
       var viz = new Viz();
       var self= this;
@@ -105,8 +86,47 @@ var app = new Vue({
           
           self.graphOut = error;
         });
-    }
 
+    }
+  },
+  
+  // https://vuejs.org/v2/guide/computed.html#Computed-vs-Watched-Property
+  computed: {
+    
+    "dotFromLambda": function () {
+      if(this.jsOut == "") { return }
+      
+      var dot_in = this.jsOut;
+      //dot_in = 'digraph { a -> b }' // for testing
+      //dot_in = pred_arrow_dot // for testing
+      
+      try {
+        dot_in = eval(dot_in);
+        var lambda_dict = lr.jsjson2dict(esprima.parse(dot_in.toString()));
+        console.log("lambda dict", lambda_dict)
+        var lambda_dot = lr.dict2dot_main(lambda_dict)
+        console.log("lambda dot", lambda_dot)
+      }
+      catch (e) {
+        // statements to handle any exceptions
+        document.getElementById("graph_out").innerHTML = e;
+        return
+      }
+      
+      this.dotOut1 = lambda_dot;
+      return lambda_dot;
+      //document.getElementById("dot_out").innerHTML = lambda_dot;
+      //dotChange()
+    }
+    
+  },
+  
+  watch: {
+    
+    "dotOut1": function() {
+      this.dotOut2 = this.dotOut1
+      this.processDot()
+    }
 
   }
 })
